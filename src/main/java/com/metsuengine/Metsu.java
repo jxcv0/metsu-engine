e696803e253c42952497e2578251c6737815ca37
jxcv0 <jmicoud02@gmail.com>
2021-09-04 17:22:30 +0100

    no message

--- src/main/java/com/metsuengine/Metsu.java
+++ src/main/java/com/metsuengine/Metsu.java
@@ -5,20 +5,26 @@ import java.time.ZonedDateTime;
 
 public class Metsu {
     public static void main( String[] args ){
+        
+        while (true) {
+            Snapshot snapshot = new Snapshot(
+                ZonedDateTime.now(ZoneOffset.UTC),
+                BybitEndpoint.getLiquidations("BTCUSD", ZonedDateTime.now(ZoneOffset.UTC).minusMinutes(1), "Buy"),
+                BybitEndpoint.getOrderBook("BTCUSD"));
 
-        Snapshot snapshot = new Snapshot(
-            ZonedDateTime.now(ZoneOffset.UTC),
-            BybitEndpoint.getLiquidations("BTCUSD", ZonedDateTime.now(ZoneOffset.UTC).minusMinutes(1), "Buy"),
-            BybitEndpoint.getOrderBook("BTCUSD"));
+            String[] lines = {
+                snapshot.getTime().toString(),
+                Double.toString(snapshot.getTotalLiquidations("Buy")),
+                Double.toString(snapshot.getOrderBook().getDeltaRatio()),
+                Double.toString(snapshot.getOrderBook().getBestAsk())
+            };
 
-        String[] lines = {
-            Double.toString(snapshot.getTotalLiquidations("Buy")),
-            Double.toString(snapshot.getOrderBook().getDeltaRatio())
-        };
-
-        CSVManager.writeLine("testing_data.csv", lines);
-
-        System.out.println(snapshot.getTotalLiquidations("Buy"));
-        System.out.println(snapshot.getOrderBook().getDeltaRatio());
+            CSVManager.writeLine("testing_data.csv", lines);
+            try {
+                Thread.sleep(1000);               
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
