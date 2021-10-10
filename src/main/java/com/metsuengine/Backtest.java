831e3b6de8b47e3c67d73c74a788d90cce45be12
jxcv0 <jmicoud02@gmail.com>
2021-10-11 00:24:54 +0100

    111021

--- src/main/java/com/metsuengine/Backtest.java
+++ src/main/java/com/metsuengine/Backtest.java
@@ -1,7 +1,6 @@
 package com.metsuengine;
 
 import java.text.DecimalFormat;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.logging.Logger;
 
@@ -47,29 +46,30 @@ public class Backtest {
             zero.addValue(DecimalNum.valueOf(0));
         }
 
+        LOGGER.info("Generating Indicators");
         ClosePriceIndicator btcClose = new ClosePriceIndicator(btcBarSeries);
         ClosePriceIndicator altClose = new ClosePriceIndicator(altBarSeries);
-        SigmaIndicator btcSigma = new SigmaIndicator(btcClose, 10000);
-        SigmaIndicator altSigma = new SigmaIndicator(altClose, 10000);
+        SigmaIndicator btcSigma = new SigmaIndicator(btcClose, 1000);
+        SigmaIndicator altSigma = new SigmaIndicator(altClose, 1000);
         DifferenceIndicator pair = new DifferenceIndicator(btcSigma, altSigma);
-        StandardDeviationIndicator deviation = new StandardDeviationIndicator(pair, 10000);
+        StandardDeviationIndicator deviation = new StandardDeviationIndicator(pair, 1000);
         BollingerBandsMiddleIndicator bbm = new BollingerBandsMiddleIndicator(zero);
         BollingerBandsLowerIndicator bbl = new BollingerBandsLowerIndicator(bbm, deviation);
         BollingerBandsUpperIndicator bbu = new BollingerBandsUpperIndicator(bbm, deviation);
         // BollingerBandsLowerIndicator tpl = new BollingerBandsLowerIndicator(bbm, deviation, DecimalNum.valueOf(0.25));
         // BollingerBandsUpperIndicator tpu = new BollingerBandsUpperIndicator(bbm, deviation, DecimalNum.valueOf(0.25));
 
-        // Strategy btcLong = new BaseStrategy("BTC", new CrossedDownIndicatorRule(pair, bbl), new CrossedUpIndicatorRule(pair, tpl));
-        // Strategy altShort = new BaseStrategy("ALT",  new CrossedDownIndicatorRule(pair, bbl), new CrossedUpIndicatorRule(pair, tpl));
+        Strategy btcLong = new BaseStrategy("BTC", new CrossedDownIndicatorRule(pair, bbl), new CrossedUpIndicatorRule(pair, bbm));
+        Strategy altShort = new BaseStrategy("ALT",  new CrossedDownIndicatorRule(pair, bbl), new CrossedUpIndicatorRule(pair, bbm));
         Strategy btcShort = new BaseStrategy("BTC",  new CrossedUpIndicatorRule(pair, bbu), new CrossedDownIndicatorRule(pair, bbm));
         Strategy altLong = new BaseStrategy("ALT",  new CrossedUpIndicatorRule(pair, bbu), new CrossedDownIndicatorRule(pair, bbm));
         
-
+        LOGGER.info("Running Strategies");
         BarSeriesManager btcSeriesManager = new BarSeriesManager(btcBarSeries);
         BarSeriesManager altSeriesManager = new BarSeriesManager(altBarSeries);
         TradingRecord btcShortRecord = btcSeriesManager.run(btcShort, TradeType.SELL);
-        // TradingRecord altShortRecord = altSeriesManager.run(altShort, TradeType.SELL);
-        // TradingRecord btcLongRecord = btcSeriesManager.run(btcLong, TradeType.BUY);
+        TradingRecord altShortRecord = altSeriesManager.run(altShort, TradeType.SELL);
+        TradingRecord btcLongRecord = btcSeriesManager.run(btcLong, TradeType.BUY);
         TradingRecord altLongRecord = altSeriesManager.run(altLong, TradeType.BUY);
 
         LOGGER.info("Building Charts");
@@ -80,14 +80,14 @@ public class Backtest {
         spreadChart.buildDataset("BBU", btcBarSeries, bbu);
         // spreadChart.buildDataset("TPL", btcBarSeries, tpl);
         // spreadChart.buildDataset("TPU", btcBarSeries, tpu);
-        // spreadChart.addMarkers(altBarSeries, altShortRecord, altShort);
+        spreadChart.addMarkers(altBarSeries, altShortRecord, altShort);
         spreadChart.addMarkers(altBarSeries, altLongRecord, altLong);
         spreadChart.displayChart();
 
         TimeSeriesChart pairChart = new TimeSeriesChart("Pair Spread Chart");
         pairChart.buildDataset("BTCUSD", btcBarSeries, btcSigma);
         pairChart.buildDataset("ALTUSD", altBarSeries, altSigma);
-        // pairChart.addMarkers(altBarSeries, altShortRecord, altShort);
+        pairChart.addMarkers(altBarSeries, altShortRecord, altShort);
         pairChart.addMarkers(altBarSeries, altLongRecord, altLong);
         pairChart.displayChart();
 
@@ -96,28 +96,28 @@ public class Backtest {
         AnalysisCriterion linearCost = new LinearTransactionCostCriterion(100, 0.00075);
         
         DecimalFormat formatter = new DecimalFormat("###.##");
-        System.out.println("Number of BTC Trades: " + btcShortRecord.getPositionCount()); // + btcLongRecord.getPositionCount());
-        System.out.println("Number of ALT Trades: " + altLongRecord.getPositionCount()); // + altShortRecord.getPositionCount());
+        System.out.println("Number of BTC Trades: " + btcShortRecord.getPositionCount() + btcLongRecord.getPositionCount());
+        System.out.println("Number of ALT Trades: " + altLongRecord.getPositionCount() + altShortRecord.getPositionCount());
         
-        // System.out.println("BTC Maximum Long Drawdown: " + formatter.format(maxDrawdown.calculate(btcBarSeries, btcLongRecord).doubleValue() * 100));
+        System.out.println("BTC Maximum Long Drawdown: " + formatter.format(maxDrawdown.calculate(btcBarSeries, btcLongRecord).doubleValue() * 100));
         System.out.println("BTC Maximum Short Drawdown: " + formatter.format(maxDrawdown.calculate(btcBarSeries, btcShortRecord).doubleValue() * 100));
         System.out.println("ALT Maximum Long Drawdown: " + formatter.format(maxDrawdown.calculate(altBarSeries, altLongRecord).doubleValue() * 100));
-        // System.out.println("ALT Maximum Short Drawdown: " + formatter.format(maxDrawdown.calculate(altBarSeries, altShortRecord).doubleValue() * 100));
+        System.out.println("ALT Maximum Short Drawdown: " + formatter.format(maxDrawdown.calculate(altBarSeries, altShortRecord).doubleValue() * 100));
 
-        // System.out.println("BTC Long Returns: " + formatter.format(grossReturn.calculate(btcBarSeries, btcLongRecord).doubleValue() * 100 - 100));
+        System.out.println("BTC Long Returns: " + formatter.format(grossReturn.calculate(btcBarSeries, btcLongRecord).doubleValue() * 100 - 100));
         System.out.println("BTC Short Returns: " + formatter.format(grossReturn.calculate(btcBarSeries, btcShortRecord).doubleValue() * 100 - 100));
         System.out.println("ALT Long Returns: " + formatter.format(grossReturn.calculate(altBarSeries, altLongRecord).doubleValue() * 100 - 100));
-        // System.out.println("ALT Short Returns: " + formatter.format(grossReturn.calculate(altBarSeries, altShortRecord).doubleValue() * 100 - 100));
+        System.out.println("ALT Short Returns: " + formatter.format(grossReturn.calculate(altBarSeries, altShortRecord).doubleValue() * 100 - 100));
 
         double tradingCost = Double.parseDouble(formatter.format(linearCost.calculate(btcBarSeries, btcShortRecord).doubleValue() 
-            // + linearCost.calculate(altBarSeries, altShortRecord).doubleValue()
-            // + linearCost.calculate(btcBarSeries, btcLongRecord).doubleValue()
+            + linearCost.calculate(altBarSeries, altShortRecord).doubleValue()
+            + linearCost.calculate(btcBarSeries, btcLongRecord).doubleValue()
             + linearCost.calculate(altBarSeries, altLongRecord).doubleValue()));
         
-        double totalReturn = (// grossReturn.calculate(btcBarSeries, btcLongRecord).doubleValue() * 100 - 100)
+        double totalReturn = (grossReturn.calculate(btcBarSeries, btcLongRecord).doubleValue() * 100 - 100)
             + (grossReturn.calculate(btcBarSeries, btcShortRecord).doubleValue() * 100 - 100)
-            + (grossReturn.calculate(altBarSeries, altLongRecord).doubleValue() * 100 - 100));
-            // + (grossReturn.calculate(altBarSeries, altShortRecord).doubleValue() * 100 - 100);
+            + (grossReturn.calculate(altBarSeries, altLongRecord).doubleValue() * 100 - 100)
+            + (grossReturn.calculate(altBarSeries, altShortRecord).doubleValue() * 100 - 100);
 
         System.out.println("Total trading Cost (%): " + tradingCost);
         System.out.println("Total returns (inc. cost): " + (totalReturn - tradingCost));
