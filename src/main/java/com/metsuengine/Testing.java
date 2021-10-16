37df6e821f765d4e85ee38eb6f172d04d55d14f2
jxcv0 <jmicoud02@gmail.com>
2021-10-16 23:59:08 +0100

    Cointegration started

--- src/main/java/com/metsuengine/Testing.java
+++ src/main/java/com/metsuengine/Testing.java
@@ -7,18 +7,19 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 public class Testing {
     public static void main(String[] args) {
 
-        TimeSeriesChart chart = new TimeSeriesChart("Testing Chart", false);
 
         final TickSeries usd = new TickSeries("usd");
         final TickSeries usdt = new TickSeries("usdt");
         final DifferenceIndicator differenceIndicator = new DifferenceIndicator("Difference", usd, usdt);
+        
+        TimeSeriesChart chart = new TimeSeriesChart("Testing Chart", false);
+        chart.addTickSeries(usd);
+        chart.addTickSeries(usdt);
+        chart.addIndicator(differenceIndicator);
+
         CSVManager usdManager = new CSVManager("BTCUSD2021-10-15.csv", usd);
         CSVManager usdtManager = new CSVManager("BTCUSDT2021-10-15.csv", usdt);
         usdManager.simulateTrades();
         usdtManager.simulateTrades();
-        chart.displayChart();
-        
-        DescriptiveStatistics descStat = new DescriptiveStatistics(differenceIndicator.toArray());
-        System.out.println(descStat.getMean());
     }
 }
