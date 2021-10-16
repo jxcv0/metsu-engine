2b45f0e9f65b05c40ed7fcb05a1af47050c12bed
jxcv0 <jmicoud02@gmail.com>
2021-10-16 19:37:30 +0100

    Charting improved

--- src/main/java/com/metsuengine/TimeSeriesChart.java
+++ src/main/java/com/metsuengine/TimeSeriesChart.java
@@ -71,7 +71,7 @@ public class TimeSeriesChart extends JFrame implements ChangeListener {
         valueAxis.setAutoRange(true);
         valueAxis.setAutoRangeIncludesZero(false);
         NumberAxis domainAxis = new NumberAxis("Time (epoch second)");
-        domainAxis.setAutoRange(true);
+        domainAxis.setFixedAutoRange(100000);
         domainAxis.setAutoRangeIncludesZero(false);
 
         plot.setRangeAxis(0, priceAxis);
