64f503d6a16640e2101183ff38587fe119843101
jxcv0 <jmicoud02@gmail.com>
2021-10-16 14:50:45 +0100

    161021

--- src/test/java/com/metsuengine/ToPriceArrayTest.java
+++ src/test/java/com/metsuengine/ToPriceArrayTest.java
@@ -3,13 +3,16 @@ package com.metsuengine;
 import static org.junit.Assert.assertArrayEquals;
 
 import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.junit.Test;
 
 public class ToPriceArrayTest {
     
     @Test
-    public void toArrayTest() {
+    public void toPriceArrayTest() {
 
         TickSeries tickSeries = new TickSeries();
         tickSeries.addTick(new Tick(
@@ -35,6 +38,21 @@ public class ToPriceArrayTest {
 
         double[] expecteds = {40000.5, 40001, 40000.5};
         double[] actuals = tickSeries.toPriceArray();
+
+        assertArrayEquals(expecteds, actuals, 0);
+    }
+
+    @Test
+    public void toArrayTest() {
+        Map<ZonedDateTime, Double> values = new ConcurrentHashMap<ZonedDateTime, Double>();
+        values.put(ZonedDateTime.now().minusMinutes(2), 10.0);
+        values.put(ZonedDateTime.now().minusMinutes(1), 20.0);
+        values.put(ZonedDateTime.now(), 30.0);
+
+        // method works but values are sometimes out of order. Not a problem for implimentaion. false negative?
+        List<Double> listActuals = values.values().stream().parallel().toList();
+        double[] actuals = listActuals.stream().mapToDouble(Double::doubleValue).toArray();
+        double[] expecteds = {10.0, 20.0, 30.0};
 
         assertArrayEquals(expecteds, actuals, 0);
     }
