00d96dca2cccd91d768ff2fef56fd906c78f95f1
jxcv0 <jmicoud02@gmail.com>
2021-10-22 18:28:46 +0100

    Websocket for orderbook started

--- src/main/java/com/metsuengine/BybitInversePerpetualOrderBookWebsocket.java
+++ src/main/java/com/metsuengine/BybitInversePerpetualOrderBookWebsocket.java
@@ -1,11 +1,56 @@
 package com.metsuengine;
 
-public class BybitInversePerpetualOrderBookWebsocket {
+import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ClientEndpoint
+public class BybitInversePerpetualOrderBookWebsocket implements WebSocket{
     
     private final MarketOrderBook orderBook;
 
-    public BybitInversePerpetualOrderBookWebsocket() {
-        this.orderBook = new MarketOrderBook();
+    public BybitInversePerpetualOrderBookWebsocket(MarketOrderBook orderBook) {
+        this.orderBook = orderBook;
     }
 
+    @OnOpen
+    public void onOpen(Session session) {
+        LOGGER.info("Connected to Inverse Perpetual endpoint: " + session.getRequestURI() + " " + session.getBasicRemote());
+        try {
+            BybitWebSocketClient.session = session;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnMessage
+    public void proccessMessage(String message) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode response = mapper.readTree(message);
+
+            if (response.has("type")) {
+                JsonNode data = response.findValue("data");
+
+                if (data)
+
+                if (!data.isNull()) {
+                    Tick tick = new Tick(
+                        data.findValue("timestamp").asText(),
+                        data.findValue("side").asText(),
+                        data.findValue("price").asDouble(),
+                        data.findValue("size").asDouble());
+                    
+                    tickSeries.addTick(tick);
+                }
+            }
+  
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
 }
